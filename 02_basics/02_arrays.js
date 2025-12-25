52e5e02615c1587606bc9b3cc6fb40e8ae4db19a
Saieshborse31 <144483421+Saieshborse31@users.noreply.github.com>
2025-12-25 12:12:59 +0000

    arrays basic

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -10,5 +10,5 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros);
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros] //
+const all_new_heros = [...marvel_heros, ...dc_heros] //spread operator;
 console.log(all_new_heros);
