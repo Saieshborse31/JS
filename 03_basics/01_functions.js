1f2c0fab370789e8709131e7dcf02962a7a3ecaf
Saiesh D Borse <144483421+Saieshborse31@users.noreply.github.com>
2026-01-06 13:13:37 +0000

    function part 2 done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,5 +1,9 @@
+
+// const { use } = require("react");
+// const React = require("react");
+
 // Create a function named sayMyName.
-function sayMyName(){
+function sayMyName() {
     console.log("S");
     console.log("A");
     console.log("I");
@@ -10,7 +14,7 @@ function sayMyName(){
 
 // sayMyName(); 
 
-function addTwoNumbers(number1, number2){
+function addTwoNumbers(number1, number2) {
     console.log(number1 + number2);
 }
 
@@ -25,24 +29,24 @@ function addTwoNumbers(number1, number2){
 // console.log("Result is: " +result); // undefined
 
 
-function addTwoNumbers(number1, number2){
+function addTwoNumbers3(number1, number2) {
     // let result = number1 + number2;
     // return result;   \
-    return number1 + number2;   
+    return number1 + number2;
 }
 
-const result = addTwoNumbers(5, 8);
+const result = addTwoNumbers3(5, 8);
 
 // console.log("Result is: " +result);  
 
-function loginUseMessage(username = "Guest"){
+function loginUseMessage(username = "Guest") {
     // if(username === undefined){
     //     return "Please enter a valid username";
     // }
-    if(!username){ //he pan check karte ki username defined ahe ki nahi
+    if (!username) { //he pan check karte ki username defined ahe ki nahi
         return "Please enter a valid username";
     }
-    else{
+    else {
         return `${username} just logged in`; // $ is used in JavaScript to insert a variable’s value inside a string.
     }
 }
@@ -51,9 +55,41 @@ function loginUseMessage(username = "Guest"){
 // loginUseMessage("Saiesh"); //run nahi honar because, return value is not stored or used anywhere
 
 // console.log(loginUseMessage(""));
-console.log(loginUseMessage("Saiesh"));
+// console.log(loginUseMessage("Saiesh"));
 // console.log(loginUseMessage()); 
 
 // undefined just logged in
 //jar kahi argument pass nahi kela tar default value "Guest" ghenar ani jar kahi argument pass kela tar to override karel default value la.
 // ani jar empty string pass kela tar to pan valid value nahi mhanun check karel ani "Please enter a valid username" return karel.
+
+function calculateCartPrice(val1, val2,...num1) { //... ha rest operator ahe mhanje aplyala jevdhe pahije tevhde argument gheu shakto
+    return num1; //will return in the form array
+}
+
+// console.log(calculateCartPrice(2, 2, 3, 4, 5));
+
+
+
+const user = {
+    username: "saiesh",
+    price: 200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+// handleObject({
+//     username: "sam",
+//     price: 500
+// });
+
+
+const myNewArray = [200,400,100,600];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+// console.log((returnSecondValue(myNewArray)));
